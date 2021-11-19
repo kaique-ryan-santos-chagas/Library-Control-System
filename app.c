e73928c9a560b447cb42cce6c29aa6c4a4a04934
= <=>
2021-11-19 20:26:42 -0300

    Trabalhando no desenvolvimento do sistema de controle de biblioteca.

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int opcao;
+
+    printf("\n");
+    printf("Sistema de controle de biblioteca\n");
+    printf("=================================\n");
+    printf("\n");
+
+    printf("1 - Listar todos os livros da biblioteca.\n");
+    printf("2 - Listar livros de uma categoria.\n");
+    printf("3 - Reservar um livro. \n");
+    printf("4 - Devolver livro. \n");
+    printf("5 - Doar livro. \n");
+
+    printf("\n");
+    printf("Escolha uma opcao: ");
+    printf("\n");
+
+    return 0;
+}
